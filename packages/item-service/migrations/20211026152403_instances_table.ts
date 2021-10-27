55a11ff3eb6a2a2cd250c071262f2b33c751e40f
Thomas Klein <thomas.klein@jwalab.com>
2021-10-27 14:28:27 +0200

    * Replaced Knex seeds with migrations * Init container to run migrations

---
+++ packages/item-service/migrations/20211026152403_instances_table.ts
@@ -0,0 +1,19 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema
+        .createTable("items_instances", function (table) {
+            table.integer("instance_number").notNullable();
+            table.integer("item_id").notNullable();
+            table.string("user_id").notNullable();
+            table.jsonb("data").notNullable().defaultTo("{}");
+            table.string("tezos_contract_address");
+            table.string("tezos_block");
+            table.primary(["instance_number", "item_id"]);
+        });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
