f0d2ebc7cdd5f13004fad9755c1b11409eb384e7
Thomas Klein <thomas.klein@jwalab.com>
2021-11-02 09:44:17 +0100

    minor improvments

--- packages/item-service/migrations/20211026152014_items_table.ts
+++ packages/item-service/migrations/20211026152014_items_table.ts
@@ -1,29 +1,25 @@
 import { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
     await knex.raw(
         "CREATE OR REPLACE FUNCTION flatten_item_data(data jsonb, name text) RETURNS TEXT AS $$ SELECT CONCAT(string_agg(value, ' '), ' ', name) FROM (SELECT value FROM JSONB_EACH_TEXT(data)) AS foo $$ LANGUAGE SQL IMMUTABLE;"
     );
 
-    await knex.schema
-        .createTable("items", function (table) {
-            table.increments("item_id");
-            table.string("studio_id").notNullable();
-            table.string("name", 100).notNullable();
-            table.integer("total_quantity").notNullable();
-            table.integer("available_quantity").notNullable();
-            table.boolean("frozen").notNullable();
-            table.jsonb("data").notNullable().defaultTo("{}");
-            table.specificType(
-                "fulltext",
-                "TEXT GENERATED ALWAYS AS (flatten_item_data(data, name)) STORED"
-            );
-            table.string("tezos_contract_address");
-            table.string("tezos_block");
-        });
+    await knex.schema.createTable("items", function (table) {
+        table.increments("item_id");
+        table.string("studio_id").notNullable();
+        table.string("name", 100).notNullable();
+        table.integer("total_quantity").notNullable();
+        table.integer("available_quantity").notNullable();
+        table.boolean("frozen").notNullable();
+        table.jsonb("data").notNullable().defaultTo("{}");
+        table.specificType(
+            "fulltext",
+            "TEXT GENERATED ALWAYS AS (flatten_item_data(data, name)) STORED"
+        );
+        table.string("tezos_contract_address");
+        table.string("tezos_block");
+    });
 }
 
-
-export async function down(knex: Knex): Promise<void> {
-}
+export async function down(knex: Knex): Promise<void> {}
--- packages/item-service/migrations/20211026152403_instances_table.ts
+++ packages/item-service/migrations/20211026152403_instances_table.ts
@@ -1,19 +1,15 @@
 import { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema
-        .createTable("items_instances", function (table) {
-            table.integer("instance_number").notNullable();
-            table.integer("item_id").notNullable();
-            table.string("user_id").notNullable();
-            table.jsonb("data").notNullable().defaultTo("{}");
-            table.string("tezos_contract_address");
-            table.string("tezos_block");
-            table.primary(["instance_number", "item_id"]);
-        });
+    await knex.schema.createTable("items_instances", function (table) {
+        table.integer("instance_number").notNullable();
+        table.integer("item_id").notNullable();
+        table.string("user_id").notNullable();
+        table.jsonb("data").notNullable().defaultTo("{}");
+        table.string("tezos_contract_address");
+        table.string("tezos_block");
+        table.primary(["instance_number", "item_id"]);
+    });
 }
 
-
-export async function down(knex: Knex): Promise<void> {
-}
+export async function down(knex: Knex): Promise<void> {}
